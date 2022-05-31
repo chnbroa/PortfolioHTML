fc642a973d2c168ee9f86d5cdd79bb885ed421e9
chnbroa <chnbroa011@naver.com>
2022-05-31 18:36:00 +0900

    remove JS

--- js/Home.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-
-const home = document.querySelector(".home__container");
-const homeHeight = home.getBoundingClientRect().height;
-
-document.addEventListener('scroll', () => {
-  home.style.opacity = 1 - window.scrollY / homeHeight
-});
